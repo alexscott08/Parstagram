5b10d855144ef4bf471a594240d4df2154f4316d
Alex Scott <axscott@fb.com>
2020-07-09 18:36:26 -0400

    Implemented endless scroll on timeline. Still glitchy on profile page

--- app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.instagramclone.EndlessRecyclerViewScrollListener;
 import com.example.instagramclone.Post;
 import com.example.instagramclone.PostsAdapter;
 import com.example.instagramclone.R;
@@ -24,6 +25,7 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static com.parse.Parse.getApplicationContext;
@@ -34,7 +36,9 @@ public class PostsFragment extends Fragment {
     private RecyclerView postsRecyclerView;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
+    private EndlessRecyclerViewScrollListener scrollListener;
     SwipeRefreshLayout swipeLayout;
+    private Date oldestPost;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -57,17 +61,26 @@ public class PostsFragment extends Fragment {
         // set the adapter on the recycler view
         postsRecyclerView.setAdapter(adapter);
         // set the layout manager on the recycler view
-        postsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        postsRecyclerView.setLayoutManager(linearLayoutManager);
         queryPosts();
+        // Retain an instance so that you can call `resetState()` for fresh searches
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                queryNewPosts();
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        postsRecyclerView.addOnScrollListener(scrollListener);
         // Getting SwipeContainerLayout
         swipeLayout = view.findViewById(R.id.swipeContainer);
         // Adding Listener
         swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                // Your code here
                 queryPosts();
-                Toast.makeText(getApplicationContext(), "Works!", Toast.LENGTH_LONG).show();
                 // To keep animation for 4 seconds
                 new Handler().postDelayed(new Runnable() {
                     @Override public void run() {
@@ -110,10 +123,46 @@ public class PostsFragment extends Fragment {
                 for (Post post : posts) {
                     Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
                 }
-
                 // update adapter with posts list
                 adapter.clear();
                 adapter.addAll(posts);
+                if (adapter.getItemCount() > 0) {
+                    oldestPost = posts.get(posts.size() - 1).getCreatedAt();
+                }
+            }
+        });
+    }
+
+    // To append new data with endless scroll listener
+    protected void queryNewPosts() {
+        // specify what type of data we want to query - Post.class
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // include data referred by user key
+        query.include(Post.KEY_USER);
+        // limit query to latest 20 items
+        query.setLimit(20);
+        // order posts by creation date and ensures only old posts not currently shown are added
+        query.whereGreaterThan("createdAt", oldestPost).orderByDescending(Post.KEY_CREATED_KEY);
+
+        // start an asynchronous call for posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // for debugging purposes let's print every post description to logcat
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                // update adapter with posts list
+                adapter.clear();
+                adapter.addAll(posts);
+                if (adapter.getItemCount() > 0) {
+                    oldestPost = posts.get(posts.size() - 1).getCreatedAt();
+                }
             }
         });
     }
--- app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
@@ -8,9 +8,12 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.Date;
 import java.util.List;
 
 public class ProfileFragment extends PostsFragment {
+
+    private Date oldestPost;
 
     @Override
     protected void queryPosts() {
@@ -32,16 +35,54 @@ public class ProfileFragment extends PostsFragment {
                     Log.e(TAG, "Issue with getting posts", e);
                     return;
                 }
-
                 // for debugging purposes let's print every post description to logcat
                 for (Post post : posts) {
                     Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
                 }
-
                 // save received posts to list and notify adapter of new data
                 adapter.clear();
                 adapter.addAll(posts);
+                if (adapter.getItemCount() > 0) {
+                    oldestPost = posts.get(posts.size() - 1).getCreatedAt();
+                }
             }
         });
     }
+
+    // To append new data with endless scroll listener
+    @Override
+    protected void queryNewPosts() {
+        // specify what type of data we want to query - Post.class
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // include data referred by user key
+        query.include(Post.KEY_USER);
+        // limit query to latest 20 items
+        query.setLimit(20);
+        // order posts by creation date and ensures only old posts not currently shown are added
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser()).
+                whereGreaterThan("createdAt", oldestPost).orderByDescending(Post.KEY_CREATED_KEY);
+
+        // start an asynchronous call for posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // for debugging purposes let's print every post description to logcat
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                // update adapter with posts list
+                adapter.clear();
+                adapter.addAll(posts);
+                if (adapter.getItemCount() > 0) {
+                    oldestPost = posts.get(posts.size() - 1).getCreatedAt();
+                }
+            }
+        });
+    }
+
 }
